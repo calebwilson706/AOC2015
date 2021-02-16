669961dc5be44c98689e5f5d513a034ac5f2879c
Caleb Wilson <calebwilson706@gmail.com>
2021-02-16 20:52:48 +0000

    project setup

---
+++ 2015ktSolutions/src/Day1.kt
@@ -0,0 +1,5 @@
+private val floorPlan = File("/Users/calebjw/Documents/AdventOfCode/2015/Day1Input.txt").readText(Charsets.UTF_8)
+
+fun solveday1() {
+    print(floorPlan)
+}
---
+++ 2015ktSolutions/src/Main.kt
@@ -0,0 +1,4 @@
+
+fun main() {
+    solveDay1()
+}
